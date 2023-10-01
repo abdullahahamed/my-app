d3866c2983387ed1b1f4f04f9861dc648fccbdc4
abdullahahamed <abdukllllah@gmail.com>
2023-10-01 22:07:45 +0500

    Initial commit

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateForm from './createForm'
+
+export default function page() {
+  return (
+    <main>
+        <h2>Add a new ticket</h2>
+        <CreateForm/>
+    </main>
+  )
+}
+
